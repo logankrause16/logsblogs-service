54a8bd662d1610c549e9cb72a31745b9399612ce
Logan Krause <logankruase16@gmail.com>
2023-02-26 10:47:23 -0500

    Fixing some goodies

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
+import json
+# pylint: disable=import-error
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app, support_credentials=False)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-@app.route("/black-series-scrapper")
+@app.route("/v1/api/")
+def home():
+    """Return the initial home page data
+    """
+    data = { "initial_message": "Hello World!"}
+    return_data = json.dumps(data)
+    return return_data
+
+@app.route("/v1/api/black-series-scrapper")
 def scrape_page():
+    """Scrape the black series goodies
+    """
     return "WIP"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
