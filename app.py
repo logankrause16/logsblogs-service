dd3c8a0d696dc74696a62ec7b077cafea502da7d
Logan Krause <logankruase16@gmail.com>
2023-02-27 11:49:37 -0500

    updating for mongo support

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import json
 # pylint: disable=import-error
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
+from pymongo import MongoClient
+
+
 
 app = Flask(__name__)
 CORS(app, support_credentials=False)
+client = MongoClient('localhost', 27017)
+
+db = client.flask_db
+todos = db.todos
 
 
 @app.route("/v1/api/")
@@ -20,7 +27,6 @@ def scrape_page():
     """Scrape the black series goodies
     """
     return "WIP"
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
