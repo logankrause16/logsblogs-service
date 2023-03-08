327c90094eddbbe55ba582c69436225094f51f68
Logan Krause <logankruase16@gmail.com>
2023-03-08 18:35:28 -0500

    Adding new endpoints

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # pylint: disable=import-error, missing-module-docstring
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from mongodb_config import mongo_db_config
 from pymongo import MongoClient
+from mongodb_config import mongo_db_config
 
+print(mongo_db_config.get('DB_URI'))
 
 app = FastAPI()
 client = MongoClient(mongo_db_config.get('DB_URI'))
@@ -29,5 +30,29 @@ async def main_page():
 def get_all():
     """
     Get everything
+    """
+    db = client['mydatabase']
+    collection = db['mycollection']
+
+
+@app.get("/v1/api/black-series/get-by-id")
+def get_by_id():
+    """
+    Get by id
+    """
+    pass
+
+@app.get("/v1/api/black-series/get-by-name")
+def get_by_name():
+    """
+    Get by name
+    """
+    pass
+
+# Post request
+@app.post("/v1/api/black-series/add")
+def add():
+    """
+    Add a new item
     """
     pass
